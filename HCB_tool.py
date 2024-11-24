2d22d5f1e4ffedff47e984ea056b4fb7ecad9815
jyx <jyxjyx1234@outlook.com>
2024-11-24 16:51:59 +0800

    1

--- HCB_tool.py
+++ HCB_tool.py
@@ -28,11 +28,8 @@ class HCB_Reader(BytesIO):
         string_out.write(text + "\n")
         return text
     
-    def readOP(self):
+    def readOP(self, encoding = "932"):
         res = f"@{self.tell()+4}::"
-        #if self.tell() - 4 in flag_list:
-        #    res += f"##flag_{flag_list.index(self.tell()-4)}\n"
-
         op = from_bytes(self.read(1))
         try:
             func = OPList[op][2]
@@ -41,7 +38,7 @@ class HCB_Reader(BytesIO):
             print(f"op : {op} not found")
             print(self.tell())
             exit()
-        res += name + " " + func(self, "d", None) + "\n"
+        res += name + " " + func(self, "d", None, encoding=encoding) + "\n"
         return res
 
 class HCB_Decompiler():
@@ -79,18 +76,20 @@ class HCB_Decompiler():
 
         with open(outpath, "w", encoding = self.encoding) as f:
             while self.data.tell() < len(self.data.getbuffer()):
-                f.write(self.data.readOP())
+                f.write(self.data.readOP(encoding=self.encoding))
             f.write(self.read_info())
 
 class HCB_Compiler():
-    def __init__(self, path) -> None:
-        data = open(path, "r", encoding="932").readlines()
+    def __init__(self, path, encoding="932") -> None:
+        data = open(path, "r", encoding=encoding).readlines()
         print("Reading file...")
         self.command_list = []
         for i in range(len(data)):
             #print(i, end="\r")
             if data[i] == "####INFO####\n":
                 break
+            if data[i] == "\n":
+                continue
             offset, command = data[i].split("::")
             offset = offset[1:]
             command = command[:-1].split(" ", 1)
@@ -109,26 +108,27 @@ class HCB_Compiler():
             self.imports.append((type[:-1], func))
             i += 1
 
-    def gen_offset_dict(self):
-        print("Generating offset dict...")
+    def gen_offset_dict(self, encoding = "932"):
+        print("Precompiling...")
         self.offset_dict = {}
         length = 4
         for offset, command in self.command_list:
             self.offset_dict[int(offset)] = length
             opname, arg = command
             opcode, func = opdict_funcname[opname]
-            length += 1 + func(arg, "l", None)
+            length += 1 + func(arg, "l", None, encoding = encoding)
+        #save_json("offset_dict.json", self.offset_dict)
     
-    def compile(self, outpath):
+    def compile(self, outpath, encoding = "932"):
+        self.gen_offset_dict(encoding = encoding)
         print("Compiling...")
-        self.gen_offset_dict()
         out = []
         for offset, command in self.command_list:
             #print(len(out), end="\r")
             opname, arg = command
             opcode, func = opdict_funcname[opname]
             try:
-                out.append(to_bytes(opcode, 1) + func(arg, "c", self.offset_dict))
+                out.append(to_bytes(opcode, 1) + func(arg, "c", self.offset_dict, encoding = encoding))
             except:
                 print(f"Error at {offset}")
                 print(f"opname : {opname}")
@@ -139,21 +139,36 @@ class HCB_Compiler():
         infos = b""
         infos += to_bytes(self.offset_dict[self.entrypoint], 4)
         infos += self.bin
-        infos += to_bytes(len(self.title.encode("932")) + 1, 1) + self.title.encode("932") + b"\x00"
+        infos += to_bytes(len(self.title.encode(encoding)) + 1, 1) + self.title.encode(encoding) + b"\x00"
         infos += to_bytes(self.num_imports, 2)
         for type, func in self.imports:
-            infos += to_bytes(int(type), 1) + to_bytes(len(func.encode("932")) + 1, 1) + func.encode("932") + b"\x00"
+            infos += to_bytes(int(type), 1) + to_bytes(len(func.encode(encoding)) + 1, 1) + func.encode(encoding) + b"\x00"
         out = length + out + infos + b"\x00\x00"
         save_file_b(outpath, out)
+        print("Done")
 
 if __name__ == "__main__":
-    hcb = "AstralAirFinale"
-    encoding = "932"
-    mode = "c"
+    import sys
+    def help():
+        print("Usage : python HCB_FILE.py <mode> <hcbfile> <encoding>")
+        print("mode : d for decompile, c for compile")
+        print("In decompile mode, the output will be a text file with the same name as the hcb file, and a _string.txt file containing all the strings (this file is just for debug, not used by the compiler)")
+        print("In compile mode, the input file will be compiled to a \"bch\" file with the same name as the input file")
+        print("hcbfile : path of the hcb file")
+        print("encoding : encoding of the text file:932 for shift-jis, 936 for gbk")
+        print("Example : python HCB_tool.py d snow.hcb 932")
+    try:
+        _, mode, hcbfile, encoding = sys.argv
+    except:
+        help()
+        exit()
+    hcb = ".".join(hcbfile.split(".")[:-1])
     if mode == "d":
-        init = open(f"{hcb}_string.txt", "w", encoding=encoding)
+        init = open(f"{hcb}_string.txt", "w", encoding = encoding)
         init.close()
-        string_out = open(f"{hcb}_string.txt", "a", encoding=encoding)
-        HCB_Decompiler(f"{hcb}.hcb", encoding=encoding).decompile(f"{hcb}.txt")
-    if mode == "c":
-        HCB_Compiler(f"{hcb}.txt").compile(f"{hcb}.bch")
+        string_out = open(f"{hcb}_string.txt", "a", encoding = encoding)
+        HCB_Decompiler(f"{hcb}.hcb", encoding = encoding).decompile(f"{hcb}.txt")
+    elif mode == "c":
+        HCB_Compiler(f"{hcb}.txt", encoding = encoding).compile(f"{hcb}.bch", encoding = encoding)
+    else:
+        help()
